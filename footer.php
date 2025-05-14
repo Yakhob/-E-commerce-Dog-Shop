afe42737b739ea0b5e602cc2d8141de1a3ee3a77
Yakhob <jacobkurian2004@gmail.com>
2025-05-14 22:25:15 +0530

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,12 @@
+<footer>
+    <div class="footer-content">
+        <p>&copy; <?php echo date("Y"); ?> Wagging Wonders. All rights reserved.</p>
+        <div class="footer-links">
+            <link rel="stylesheet" href="assets/css/footer.css">
+
+        </div>
+        <p class="footer-description">Since our establishment in 2024, Wagging Wonders has become a trusted name in the pet care industry. Offering a wide range of dogs for adoption, pet grooming services, and high-quality pet food and supplies, we are dedicated to meeting the needs of pet owners. With our growing online platform, we continue to serve pet lovers across the country, providing exceptional care and service for both pets and their owners.</p>
+    </div>
+</footer>
+</body>
+</html>
